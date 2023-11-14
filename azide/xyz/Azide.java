5ec66b2b27232743379b9194ce0dbc708fe183b0
Plusbox <111814410+vykngs@users.noreply.github.com>
2023-11-14 14:55:30 -0800

    Update Azide.java

--- azide/xyz/Azide.java
+++ azide/xyz/Azide.java
@@ -4,7 +4,7 @@ import org.lwjgl.opengl.Display;
 
 public final class Azide {
     private static final Azide SINGLETON;
-    public static final String NAME = "Azide", VERSION = "1.0";
+    private static final String NAME, VERSION;
 
     private Azide() {
 
@@ -12,10 +12,6 @@ public final class Azide {
 
     public static Azide getSingleton() {
         return SINGLETON;
-    }
-
-    public void init() {
-        Display.setTitle(NAME + " " + VERSION);
     }
 
     public static String getName() {
@@ -26,7 +22,13 @@ public final class Azide {
         return VERSION;
     }
 
+    public void init() {
+        Display.setTitle(NAME + " " + VERSION);
+    }
+
     static {
         SINGLETON = new Azide();
+        NAME = "Azide";
+        VERSION = "0.1";
     }
 }
